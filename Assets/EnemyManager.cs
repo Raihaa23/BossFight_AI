022f092abff2615a637e4e76a31b5c26898fc1f6
Ryan Cedullo <95852131+Raihaa23@users.noreply.github.com>
2023-04-18 20:53:34 +0800

    Continuation of Boss Fight

--- Assets/EnemyManager.cs
+++ Assets/EnemyManager.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EnemyManager : MonoBehaviour
-{
+public class EnemyManager : MonoBehaviour {
     public Image hpBar;
     public float maxHP;
     public float curHP;
+    public ParticleSystem meleeExplode;
+
+    public int meleeCounter;
+    public int heavyCounter;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         hpBar.fillAmount = curHP / maxHP;
+    }
+
+    public void MeleeExplosion() {
+        Debug.Log("Boom");
+        meleeExplode.Play();
+    }
+
+    public void MeleeAttackCounter() 
+    {
+        meleeCounter += 1;
+    }
+    public void HeavyAttackDamage() {
+        heavyCounter += 1;
     }
 }
---
+++ Assets/MonsterChaseState.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterChaseState : StateMachineBehaviour
+{
+
+    private NavMeshAgent agent;
+    Transform player;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateinfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("player").transform;
+        agent = animator.GetComponent <NavMeshAgent>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+   override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+   {
+        agent.destination = player.position;
+		if (agent.remainingDistance < 1) {
+            animator.SetBool("isMeleeAttacking", true);
+            animator.SetBool("isChasing", false);
+		}
+   }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
